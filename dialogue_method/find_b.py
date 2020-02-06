b2eb520c07b35010c250b40470f7cd7717e33872
JvParidon <Jeroen.vanParidon@mpi.nl>
2020-02-06 15:12:39 +0100

    Small fix to dialogue method (check if B == A)

--- dialogue_method/find_b.py
+++ dialogue_method/find_b.py
@@ -1,17 +1,36 @@
 import argparse
 
 
-def find_b(lang, fname=None):
+def parse_line(line):
+    line = line.split(']')
+    try:
+        timestamp = float(line[0].split('[')[1].replace(' ', '').replace('-', ''))
+    except ValueError:
+        timestamp = 0
+    stripped = line[1].replace('-', '').strip(' ')
+    return stripped, timestamp
+
+
+def find_b(lang, fname=None, verbose=True):
     if fname is None:
         fname = f'{lang}_aba.txt'
     with open(fname, 'r') as abafile, open(f'{lang}_b.txt', 'w') as bfile:
-        for line in abafile:
-            if line.startswith('B'):
-                line = line.split(']')[1]
-                line = line.replace('-', '').strip('\n').strip(' ')
-                if (len(line.split(' ')) == 1) and line.endswith('?'):
-                    print(line)
-                    bfile.write(line + '\n')
+        abatext = abafile.read().split('\n')  # read text and split into list of lines
+        for i, line in enumerate(abatext):  # iterate over lines, but keep track of index
+            if line.startswith('B'):  # check if a line is a B line (in ABA sequence)
+                stripped_b, time_b = parse_line(line)  # parse B line into utterance and timestamp
+                stripped_a, time_a1 = parse_line(abatext[i - 1])  # parse A1 line into utterance and timestamp
+                stripped_a, time_a2 = parse_line(abatext[i + 1])  # parse A2 line into utterance and timestamp
+                if stripped_b != stripped_a:  # check if B line is not identical to A lines
+                    if (len(stripped_b.split(' ')) == 1) and stripped_b.endswith('?'):  # check if line is one word and ends in ?
+                        bfile.write(stripped_b + '\n')  # write to file
+
+                        if verbose:
+                            # some printing for diagnostic purposes
+                            print(abatext[i - 1])
+                            print(line)
+                            print(abatext[i + 1])
+                            print('\n')
 
 
 if __name__ == '__main__':
